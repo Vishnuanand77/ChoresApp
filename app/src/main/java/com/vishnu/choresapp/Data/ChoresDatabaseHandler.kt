a0553b53e78de4a795fa1fc01e97f6ae87500f91
Vishnu Anand <vishnuanand2000@gmail.com>
2021-05-28 20:37:25 +0530

    Implemented Insert to database function

--- app/src/main/java/com/vishnu/choresapp/Data/ChoresDatabaseHandler.kt
+++ app/src/main/java/com/vishnu/choresapp/Data/ChoresDatabaseHandler.kt
@@ -1,8 +1,10 @@
 package com.vishnu.choresapp.Data
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
 import com.vishnu.choresapp.Model.*
 
 class ChoresDatabaseHandler(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION){
@@ -21,6 +23,30 @@ class ChoresDatabaseHandler(context: Context) : SQLiteOpenHelper(context, DATABA
 
     //Invoked when the database gets upgraded
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        TODO("Not yet implemented")
+        db?.execSQL("DROP TABLE IF EXISTS "+ TABLE_NAME)
+
+        //Create Table Again
+        onCreate(db)
     }
+
+    //Functions for CRUD Operations
+
+    fun createChore(chore: Chore) {
+        var db: SQLiteDatabase = writableDatabase
+
+        var values: ContentValues = ContentValues()
+        values.put(KEY_CHORE_NAME, chore.choreName)
+        values.put(KEY_CHORE_ASSIGNED_BY, chore.assignedBy)
+        values.put(KEY_CHORE_ASSIGNED_TO, chore.assignedTo)
+        values.put(KEY_CHORE_ASSIGNED_TIME, System.currentTimeMillis())
+
+        //Invoking the SQL query
+        db.insert(TABLE_NAME, null, values)
+
+        Log.d("DATA INSERTED", "SUCCESS")
+
+        //Closing the database
+        db.close()
+    }
+
 }
