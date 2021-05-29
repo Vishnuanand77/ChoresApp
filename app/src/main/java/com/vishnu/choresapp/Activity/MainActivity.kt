51434818f6d31191778ccfbf3bb8ab44a38e6c35
Vishnu Anand <vishnuanand2000@gmail.com>
2021-05-29 21:33:50 +0530

    Initialized UI in MainActivity.kt

--- app/src/main/java/com/vishnu/choresapp/Activity/MainActivity.kt
+++ app/src/main/java/com/vishnu/choresapp/Activity/MainActivity.kt
@@ -2,13 +2,23 @@ package com.vishnu.choresapp.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
 import com.vishnu.choresapp.Data.ChoresDatabaseHandler
 import com.vishnu.choresapp.Model.Chore
 import com.vishnu.choresapp.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    //Database Handler Initialization
     var dbHandler: ChoresDatabaseHandler? = null
+
+    //XML Initializations
+    var enterChore = enterChoreEditText
+    var assignedBy = assignedByEditText
+    var assignedTo = assignedToEditText
+    var saveBtn = saveButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +26,17 @@ class MainActivity : AppCompatActivity() {
 
         dbHandler = ChoresDatabaseHandler(this)
 
+        saveBtn.setOnClickListener {
+            if (!TextUtils.isEmpty(enterChore.text.toString())
+                && !TextUtils.isEmpty(assignedBy.text.toString())
+                        && !TextUtils.isEmpty(assignedTo.text.toString())) {
+                TODO("SAVE TO DATABASE")
+            }
+        }
+
+    }
+
+    fun TestDB() {
         var chore = Chore()
         chore.choreName = "Clean Room 3"
         chore.assignedTo = "Varun"
